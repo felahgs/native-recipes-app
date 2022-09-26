df322d6d145701b95add2c5f9a34515af2629ca3
Felipe Souza <fgsouza93@gmail.com>
2022-09-26 20:46:11 -0300

    Create Splash screen with gradient

--- src/screens/Splash/Splash.tsx
+++ src/screens/Splash/Splash.tsx
@@ -1,36 +1,58 @@
 import { View, ImageBackground } from "react-native";
 import React from "react";
+import LinearGradient from "react-native-linear-gradient";
 
 import Button from "components/Button";
 import Text from "components/Text";
-import Logo from "assets/images/logo.svg";
+import LogoImage from "assets/images/logo.svg";
 import backgroundImage from "assets/images/splash-background.jpg";
+import { useTheme } from "hooks/useTheme";
 
 import styles from "./styles";
 
 const Splash = () => {
-  return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source={backgroundImage}>
-        <View>
-          <Logo />
-          <Text.Medium bold>Premium Recipe</Text.Medium>
-        </View>
+  const theme = useTheme();
 
-        <View>
-          <Text.Title>Get</Text.Title>
-          <Text.Title>Cooking</Text.Title>
-          <Text>Simple way to find Tasty Recipe</Text>
-        </View>
-
-        <Button
-          onPress={() => console.log("TODO: navigate to login screen")}
-          title="Entrar"
-          icon="arrow-right"
-          passedStyle={styles.button}
-        />
-      </ImageBackground>
+  const Logo = () => (
+    <View style={styles.logo}>
+      <LogoImage />
     </View>
+  );
+
+  return (
+    <ImageBackground style={styles.backgroundImage} source={backgroundImage}>
+      <LinearGradient colors={["#0000003b", "#000000"]} style={styles.mask} />
+
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Logo />
+          <Text.Medium color={theme.buttonLabel} bold>
+            Premium Recipe
+          </Text.Medium>
+        </View>
+
+        <View style={styles.content}>
+          <View style={styles.title}>
+            <Text.Title bold color={theme.buttonLabel}>
+              Get
+            </Text.Title>
+            <Text.Title bold color={theme.buttonLabel}>
+              Cooking
+            </Text.Title>
+            <Text color={theme.buttonLabel}>
+              Simple way to find Tasty Recipe
+            </Text>
+          </View>
+
+          <Button
+            onPress={() => console.log("TODO: navigate to login screen")}
+            title="Comerçar"
+            icon="arrow-right"
+            passedStyle={styles.button}
+          />
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
--- src/screens/Splash/styles.ts
+++ src/screens/Splash/styles.ts
@@ -1,11 +1,41 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  box: {
+    width: "100%",
+    height: 200,
+  },
   container: {
     flex: 1,
+    justifyContent: "space-between",
+    resizeMode: "cover",
+    padding: 40,
   },
-  image: {
+  mask: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  maskBottom: {
+    width: "100%",
+    height: "20%",
+    position: "absolute",
+  },
+  backgroundImage: {
     flex: 1,
     resizeMode: "cover",
+  },
+  logo: {
+    paddingBottom: 15,
+  },
+  content: {
+    alignItems: "center",
+  },
+  title: {
+    marginBottom: 60,
+    alignItems: "center",
+  },
+  button: {
+    alignSelf: "stretch",
   },
 });
